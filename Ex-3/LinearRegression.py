3d3112a6f3abce2d5bf07b969c09c890d0276db5
kmario23 <marimuthuk@live.com>
2016-11-25 21:35:31 +0100

    update linear regression code

--- Ex-3/LinearRegression.py
+++ Ex-3/LinearRegression.py
@@ -7,17 +7,28 @@ def main():
     """
     Input: auto-mpg.data
     """
-    #prepare data
+    # prepare data
     data = np.loadtxt('auto-mpg.data', usecols=(0, 2))
     Xfull, Yfull = data[:, 0], data[:, 1]
     Xtrain, Ytrain = Xfull[0:50], Yfull[0:50]
     Xtest, Ytest = Xfull[50:100], Yfull[50:100]
 
-    #plot data
-    plt.plot(Xtrain, Ytrain)
+    # plot data
+    fig = plt.figure()
+    ax = fig.gca()
+    ax.set_xticks(np.arange(5, 30, 1))
+    ax.set_yticks(np.arange(50, 500, 40))
+    plt.scatter(Xtrain, Ytrain)
     plt.xlabel('miles per gallon')
     plt.ylabel('displacement')
+    plt.grid(True)
     plt.show()
+
+    # observing the plot suggests that we need at least
+    # quadratic polynomial to appropriately fit the data
+
+    # fit using first order polynomial
+    # (w * (np.transpose(Xtrain) * Xtrain)) = (np.transpose(Xtrain) * Ytrain)
 
 
 if __name__ == "__main__":
